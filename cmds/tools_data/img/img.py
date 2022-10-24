4781113b284c91f670dc8a00ebfea6941f90825b
Gallen Wang <gallen.yes@gmail.com>
2022-10-24 22:04:52 +0800

    optimization

--- cmds/tools_data/img/img.py
+++ cmds/tools_data/img/img.py
@@ -4,11 +4,14 @@ import requests
 import function
 import os
 
+IMG_PATH = './cmds/tools_data/img/temp.png'
+
 def ocr(img_url, lang):
-    with open('./cmds/tools_data/img/temp.png', 'wb') as file:
+    
+    with open(IMG_PATH, 'wb') as file:
         file.write(requests.get(img_url, headers={'user-agent': 'Mozilla/5.0'}).content)
     function.print_time('Saved temp.png successfully')
-    text = pytesseract.image_to_string(Image.open('./cmds/tools_data/img/temp.png'), lang=lang)
+    text = pytesseract.image_to_string(Image.open(IMG_PATH), lang=lang)
     print(text)
     if text != '':
         text_list = []
@@ -26,15 +29,15 @@ def ocr(img_url, lang):
         return ['***Character not found***']
 
 def rotate(img_url, angle):
-    with open('./cmds/tools_data/img/temp.png', 'wb') as file:
+    with open(IMG_PATH, 'wb') as file:
         file.write(requests.get(img_url, headers={'user-agent': 'Mozilla/5.0'}).content)
 
-    Image.open('./cmds/tools_data/img/temp.png').rotate(angle, expand=1).save('./cmds/tools_data/img/temp.png', quality=100)
+    Image.open(IMG_PATH).rotate(angle, expand=1).save(IMG_PATH, quality=100)
     function.print_time('Saved temp.png successfully')
     for i in range(95, 0, -5):
-        if os.path.getsize('./cmds/tools_data/img/temp.png') >= 8388608:
+        if os.path.getsize(IMG_PATH) >= 8388608:
             function.print_time(f'Picture size is larger than 8MB, resave it with quality {i}')
-            Image.open('./cmds/tools_data/img/temp.png').rotate(angle, expand=1).save('./cmds/tools_data/img/temp.png', quality=i)
+            Image.open(IMG_PATH).rotate(angle, expand=1).save(IMG_PATH, quality=i)
             function.print_time('Saved temp.png successfully')
         else:
             break
