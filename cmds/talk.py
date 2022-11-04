2cff37afca66b9b29db17dcfaf0e427a9d2dd8de
Gallen Wang <gallen.yes@gmail.com>
2022-11-04 20:20:07 +0800

    add nochat

--- cmds/talk.py
+++ cmds/talk.py
@@ -5,11 +5,19 @@ import function
 class Talk(Cog_Extension):
 
     @commands.command()
-    async def talk(self, ctx, lang):
+    async def chat(self, ctx, lang):
         talk = function.open_json('./cmds/talk_data/talk.json')
         talk[str(ctx.message.channel.id)] = lang
         function.write_json('./cmds/talk_data/talk.json', talk)
         function.print_time(f'{ctx.channel}({ctx.channel.id}) chat on')
+
+    
+    @commands.command()
+    async def nochat(self, ctx):
+        talk = function.open_json('./cmds/talk_data/talk.json')
+        del talk[str(ctx.message.channel.id)]
+        function.write_json('./cmds/talk_data/talk.json', talk)
+        function.print_time(f'{ctx.channel}({ctx.channel.id}) chat off')
 
 async def setup(bot):
     await bot.add_cog(Talk(bot))
