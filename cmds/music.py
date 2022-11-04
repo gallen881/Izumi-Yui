ff3ed9cfebc2412f5cf86229efe38e834aa3c44a
Gallen Wang <gallen.yes@gmail.com>
2022-11-04 23:11:31 +0800

    pass join

--- cmds/music.py
+++ cmds/music.py
@@ -60,6 +60,9 @@ class Music(Cog_Extension):
 
     @commands.command()
     async def play(self, ctx, url):
+        if not self.bot.voice:
+            channel = ctx.message.author.voice.channel
+            await channel.connect()
         try :
             voice_client: discord.VoiceClient = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
             print(voice_client)
