0da8796f0de873922562a29e68a1dc3e2c05c8c9
Gallen Wang <gallen.yes@gmail.com>
2022-11-14 11:39:08 +0800

    add split_str and print_detail

--- function.py
+++ function.py
@@ -1,22 +1,83 @@
-def open_json(fileName):
+def open_json(FileName: str) -> dict:
     import json
-    if fileName != '':
-        strList = fileName.split('.')
+    if FileName != '':
+        strList = FileName.split('.')
         if strList[len(strList)-1].lower() == 'json':
-            with open(fileName, mode = 'r', encoding = 'utf-8') as file:
+            with open(FileName, mode = 'r', encoding = 'utf-8') as file:
                 data = json.loads(file.read())
                 file.close()
                 return data
 
-def write_json(fileName, enter):
+
+def write_json(FileName: str, dictionary: dict) -> None:
     import json
-    if fileName != '':
-        strList = fileName.split('.')
-        if strList[len(strList)-1].lower() == 'json':
-            with open(fileName, mode = 'w', encoding = 'utf-8') as file:
-                json.dump(enter, file)
+    if FileName != '':
+        StrList = FileName.split('.')
+        if StrList[len(StrList)-1].lower() == 'json':
+            with open(FileName, mode='w', encoding='utf-8') as file:
+                json.dump(dictionary, file)
                 file.close()
 
-def print_time(enter):
+
+def print_detail(memo='', user=None, guild=None, channel=None, obj=None) -> None:
     import time
-    print(f'[{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}] {enter}')
+    import colorama
+    colorama.init()
+
+    if memo == 'INFO':
+        mcolor = colorama.Fore.GREEN
+    elif memo == 'WARN':
+        mcolor = colorama.Fore.RED
+    elif memo == 'ERROR':
+        mcolor = colorama.Fore.RED + colorama.Style.BRIGHT
+    else:
+        mcolor = colorama.Fore.CYAN
+
+    class Formate:
+        name = ''
+        discriminator = ''
+        id = ''
+
+    if user == None:
+        user = Formate
+
+    if guild == None:
+        guild = Formate
+
+    if channel == None:
+        channel = Formate
+
+    def fill(string, length: int) -> str:
+        if string == '':
+            return ' ' * length
+        else:
+            import unicodedata
+            string = str(string)
+            leng = 0
+            for char in string:
+                if unicodedata.east_asian_width(char) == 'W':
+                    leng += 2
+                else:
+                    leng += 1
+        if leng < length:
+            gap = length - leng
+            string = ' ' * gap + string
+
+        return string
+ 
+
+    print(f'{colorama.Style.DIM}[{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}]{colorama.Style.RESET_ALL}{mcolor}[{fill(memo, 5)}]{colorama.Style.RESET_ALL}{colorama.Fore.BLUE}[{fill(user.name + user.discriminator, 20)}({fill(user.id, 19)})][{fill(guild.name, 20)}({fill(guild.id, 19)}][{fill(channel.name, 20)}({fill(channel.id, 19)})]{colorama.Style.RESET_ALL}\n{obj}\n')
+
+
+def split_str_to_list(text: str, size: int) -> list:
+    text_list = []
+    if text != '':
+        while True:
+            text_list.append(text[:size])
+            text = text[size + 1:]
+            if text == '':
+                break
+    else:
+        text_list = ['']
+
+    print_detail(memo='INFO', obj=f'Form list: "{text_list}"')
